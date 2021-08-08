198b0547a3dbb233a7f83a7afbea4b37617bb4e7
ojasvinigupta <guptaojas15@gmail.com>
2021-08-08 15:16:46 +0530

    WebApp for login

---
+++ main.py
@@ -0,0 +1,46 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+from passlib.hash import sha256_crypt
+import mysql.connector as mariadb
+import os
+import operator
+app = Flask(__name__)
+mariadb_connect = mariadb.connect(user='ogupta', password='Ojas@150297', database='Login')
+@app.route('/')
+def home():
+  if not session.get('logged_in'):
+    return render_template('login.html')
+  else:
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+  login = request.form
+
+  userName = login['username']
+  password = login['password']
+
+  cur = mariadb_connect.cursor(buffered=True)
+  select_stmt = "SELECT * FROM Login WHERE username = 'Jack'"
+  #data_employee = ('Jack')
+  data = cur.execute(select_stmt)
+  #data = cur.execute('SELECT * FROM Login WHERE username=%(userName)s')
+  data = cur.fetchone()[2]
+
+  if password == data:
+    account = True
+
+  if account:
+    session['logged_in'] = True
+  else:
+    flash('wrong password!')
+  return home()
+
+@app.route('/logout')
+def logout():
+  session['logged_in'] = False
+  return home()
+
+if __name__ == "__main__":
+  app.secret_key = os.urandom(12)
+  app.run(debug=False,host='0.0.0.0', port=8000)
---
+++ newUser.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from passlib.hash import sha256_crypt
+import mysql.connector as mariadb
+app = Flask(__name__)
+maraidb_connection = mariadb.connect(user='ogupta', password='Ojas@150297', database='Login')
+@app.route('/')
+def index():
+  username = "Jack"
+  password = sha256_crypt.encrypt("Jack")
+  email = "jack.com"
+
+  cur = mariadb_connection.cursor()
+  cur.execute('INSERT INTO Login (username, password, email) VALUES (%s, %s, %s)', (username, password, email))
+  mariadb_connection.commit()
+  cur.close()
+
+  return "New user added"
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0', port='8000')
+
